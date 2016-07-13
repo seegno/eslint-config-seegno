c1c47dfd39f1579e98ad37ed12564de2863be669
Rui Quelhas <quelhas@uphold.com>
2016-07-13 14:49:15 +0100

    Update rules to enable JSCS-compatible ones

---
+++ test/fixtures/correct.js
@@ -0,0 +1,232 @@
+// Avoid extra `no-unused-vars` violations.
+function noop() {
+  // do nothing
+}
+
+// `array-bracket-spacing`, `comma-spacing` and `no-multi-spaces`.
+noop(['bar', 'foo']);
+
+// `comma-dangle`, `comma-style` and `sorting/sort-object-props`.
+noop({ bar: 'foo', foo: 'bar' });
+
+// `consistent-this`.
+const self = this;
+
+noop(self);
+
+// `curly`, `keyword-spacing`, `no-empty` and `space-before-blocks`.
+let mixedRules = true;
+
+if (mixedRules) {
+  mixedRules = false;
+} else {
+  mixedRules = true;
+}
+
+// `dot-notation`.
+const dotNotation = {};
+
+dotNotation.foo = 'bar';
+
+// `id-match`.
+let idmatch;
+let idMatch;
+let IdMatch;
+let IDMatch;
+let IDMATCH;
+let ID_MATCH;
+let ID_M_ATCH;
+
+noop(idmatch);
+noop(idMatch);
+noop(IdMatch);
+noop(IDMatch);
+noop(IDMATCH);
+noop(ID_MATCH);
+noop(ID_M_ATCH);
+
+// `key-spacing`.
+noop({ foo: 'bar' });
+
+// `mocha/no-exclusive-tests`.
+describe('noExclusiveTests', () => {
+  it('should work');
+});
+
+// `new-cap`.
+const Cap = require('cap');
+const newCap = new Cap();
+
+noop(newCap);
+
+// `no-class-assign`.
+class NoClassAssign { }
+
+noop(NoClassAssign);
+
+// `no-const-assign`.
+let noConstAssign = true;
+
+noConstAssign = false;
+
+noop(noConstAssign);
+
+// `no-constant-condition`.
+const noConstantCondition = true;
+
+if (noConstantCondition) {
+  noop(noConstantCondition);
+}
+
+// `no-dupe-class-members`.
+class NoDupeClassMembers {
+  bar() { return 'bar'; }
+  foo() { return 'foo'; }
+}
+
+noop(NoDupeClassMembers);
+
+// `no-labels`.
+const noLabels = { label: true };
+
+while (noLabels.label) {
+  break;
+}
+
+// `no-multi-str`.
+const noMultiStr = `Line 1
+  Line 2`;
+
+noop(noMultiStr);
+
+// `no-multiple-empty-lines`.
+const noMultipleEmptyLines = true;
+
+noop(noMultipleEmptyLines);
+
+// `no-spaced-func`.
+noop();
+
+// `no-this-before-super`.
+const NoThisBeforeSuper = require('no-this-before-super');
+
+class Child extends NoThisBeforeSuper {
+  constructor() {
+    super();
+    this.foo = 'bar';
+  }
+}
+
+noop(Child);
+
+// `no-underscore-dangle`.
+class NoUnderscoreDangle {
+  constructor() {
+    this._foo = 'bar';
+  }
+}
+
+noop(NoUnderscoreDangle);
+
+// `object-curly-spacing`.
+const objectCurlySpacing1 = { foo: 'bar' };
+const objectCurlySpacing2 = {};
+
+noop(objectCurlySpacing1);
+noop(objectCurlySpacing2);
+
+// `one-var`.
+const oneVar1 = 'foo';
+const oneVar2 = 'bar';
+
+noop(oneVar1);
+noop(oneVar2);
+
+// `operator-linebreak`.
+const operatorLineBreak = 1 + 2;
+
+noop(operatorLineBreak);
+
+// `padded-blocks`.
+const paddedBlocks = true;
+
+if (paddedBlocks) {
+  noop();
+}
+
+// `quote-props`.
+const quoteProps = {
+  0: 0,
+  foo: 0,
+  'foo-bar': 0,
+  null: 0,
+  true: 0
+};
+
+noop(quoteProps);
+
+// `quotes`.
+const quotes1 = 'foo';
+const quotes2 = `foo`;
+
+noop(quotes1);
+noop(quotes2);
+
+// `semi`.
+noop();
+
+// `semi-spacing`.
+for (let semiSpacing = 0; semiSpacing < 10; ++semiSpacing) {
+  noop();
+}
+
+// `sort-imports`.
+import SortImport1 from 'sort-import1';
+import { sortImport2 } from 'sort-import2';
+import sortImport4 from 'sort-import4';
+import { sortImport3, sortImport5 } from 'sort-import';
+
+noop(SortImport1);
+noop(sortImport2);
+noop(sortImport3);
+noop(sortImport4);
+noop(sortImport5);
+
+// `space-before-function-paren`.
+(function() {
+  noop();
+})();
+
+// `space-in-parens`.
+noop('foo');
+
+// `space-infix-ops`.
+const spaceInfixOps = 1 + 2;
+
+noop(spaceInfixOps);
+
+// `space-unary-ops`.
+let spaceUnaryOps1 = 1;
+const spaceUnaryOps2 = ++spaceUnaryOps1;
+
+noop(spaceUnaryOps2);
+
+// `spaced-comment`.
+// spaced comment.
+
+// `template-curly-spacing`.
+const templateCurlySpacing = 'foo';
+
+noop(`${templateCurlySpacing}`);
+
+// `wrap-iife`.
+(function() {
+  noop();
+})();
+
+// `yoda`.
+let yoda = true;
+
+if (yoda === true) {
+  yoda = false;
+}
---
+++ test/fixtures/incorrect.js
@@ -0,0 +1,247 @@
+// Avoid extra `no-unused-vars` violations.
+function noop() {
+  // do nothing
+}
+
+// `array-bracket-spacing`.
+noop([ 'bar', 'foo']);
+
+// `comma-dangle`.
+noop({ bar: 'foo', foo: 'bar', });
+
+// `comma-spacing`.
+noop(['bar','foo']);
+
+// `comma-style`.
+noop({
+    bar: 'foo'
+  , foo: 'bar'
+});
+
+// `consistent-this`.
+const consistentThis = this;
+
+noop(consistentThis);
+
+// `curly`.
+let curly = true;
+
+if (curly)
+  curly = false
+
+// `dot-notation`.
+const dotNotation = {};
+
+dotNotation['foo'] = 'bar';
+
+// `id-match`.
+let id_match;
+
+noop(id_match);
+
+// `key-spacing`.
+noop({ foo:'bar' });
+
+// `keyword-spacing`.
+let keywordSpacing = true;
+
+if(keywordSpacing) {
+  keywordSpacing = false;
+}
+
+// `mocha/no-exclusive-tests`.
+describe.only('noExclusiveTests', () => {
+  it('should not work');
+});
+
+// `new-cap`.
+const cap = require('cap');
+const newCap = new cap();
+
+// `no-class-assign`.
+class NoClassAssign { }
+
+NoClassAssign = 'foobar';
+
+noop(NoClassAssign);
+
+// `no-const-assign`.
+const noConstAssign = true;
+
+noConstAssign = false;
+
+// `no-constant-condition`.
+if (true) {
+  noop(true);
+}
+
+// `no-dupe-class-members`.
+class NoDupeClassMembers {
+  foo() { return 'bar'; }
+  foo() { return 'foo'; }
+}
+
+noop(NoDupeClassMembers);
+
+// `no-empty`.
+try {
+  noop();
+} catch (e) {}
+
+// `no-labels`.
+noLabels:
+  while (noLabels) {
+    break noLabels;
+  }
+
+// `no-mixed-spaces-and-tabs`.
+const noMixedSpacesAndTabs = {
+  bar: 'foo',
+  foo: 'bar'
+};
+
+// `no-multi-spaces`.
+noop(['foo',  'bar']);
+
+// `no-multi-str`.
+const noMultiStr = 'Line 1 \
+  Line 2';
+
+noop(noMultiStr);
+
+// `no-multiple-empty-lines`.
+const noMultipleEmptyLines = true;
+
+
+noop(noMultipleEmptyLines);
+
+// `no-spaced-func`.
+noop ();
+
+// `no-this-before-super`.
+const NoThisBeforeSuper = require('no-this-before-super');
+
+class Child extends NoThisBeforeSuper {
+  constructor() {
+    this.foo = 'bar';
+    super();
+  }
+}
+
+noop(Child);
+
+// `no-underscore-dangle`.
+const noUnderscoreDangle = {};
+
+noUnderscoreDangle._foo = 'bar';
+
+noop(NoUnderscoreDangle);
+
+// `object-curly-spacing`.
+const objectCurlySpacing = {foo: 'bar'};
+
+noop(objectCurlySpacing);
+
+// `one-var`.
+const oneVar1 = 'foo', oneVar2 = 'bar';
+
+noop(oneVar1);
+noop(oneVar2);
+
+// `operator-linebreak`.
+const operatorLineBreak = 1 +
+  2;
+
+noop(operatorLineBreak);
+
+// `padded-blocks`.
+const paddedBlocks = true;
+
+if (paddedBlocks) {
+
+  noop();
+
+}
+
+// `quote-props`.
+const quoteProps = {
+  '0': 0,
+  'foo': 0,
+  'foo-bar': 0,
+  'null': 0,
+  'true': 0
+};
+
+noop(quoteProps);
+
+// `quotes`.
+const quotes = "foo";
+
+noop(quotes);
+
+// `semi`.
+noop()
+
+// `semi-spacing`.
+for (let semiSpacing = 0;semiSpacing < 10;++semiSpacing) {
+  noop();
+}
+
+// `sort-imports`.
+import { sortImport2, sortImport3 } from 'sort-import';
+import { sortImport1 } from 'sort-import1';
+
+noop(sortImport1);
+noop(sortImport2);
+noop(sortImport3);
+
+// `sorting/sort-object-props`.
+noop({ foo: 'bar', bar: 'foo' });
+
+// `space-before-blocks`.
+let spaceBeforeBlocks = true;
+
+if (spaceBeforeBlocks){
+  spaceBeforeBlocks = false;
+}else{
+  spaceBeforeBlocks = true;
+}
+
+// `space-before-function-paren`.
+(function () {
+  noop();
+})();
+
+// `space-in-parens`.
+noop( 'foo' );
+
+// `space-infix-ops`.
+const spaceInfixOps = 1+2;
+
+noop(spaceInfixOps);
+
+// `space-unary-ops`.
+let spaceUnaryOps1 = 1;
+const spaceUnaryOps2 = ++ spaceUnaryOps1;
+
+noop(spaceUnaryOps2);
+
+// `spaced-comment`.
+//Comment missing space.
+
+// `template-curly-spacing`.
+const templateCurlySpacing = 'foo';
+
+noop(`${ templateCurlySpacing }`);
+
+// `wrap-iife`.
+(function() {
+  noop();
+}());
+
+// `yoda`.
+let yoda = true;
+
+if (true === yoda) {
+  yoda = false;
+}
